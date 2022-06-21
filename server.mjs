f20b0fa4b26a98f019605772545a292763cf194e
ameted83 <tedeschiamelia@gmail.com>
2022-06-21 17:25:40 +0200

    Change the HTML response

--- server.mjs
+++ server.mjs
@@ -3,8 +3,8 @@ import {createServer} from "node:http"
 const server = createServer((request, response)=>{
     console.log("request received")
     response.statusCode = 200
-    response.setHeader("Content-Type", "tetx/html")
-    response.end("<html><body><h1>This page was sarved whith Node.js!</h1></body></html>")
+    response.setHeader("Content-Type", "text/html")
+    response.end("<html><body><h1>Welcome Node.js!</h1></body></html>")
 })
 
 server.listen(3000, ()=>{
