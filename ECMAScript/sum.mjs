c8c1c298ee854971d51832812a737ef027461bab
ameted83 <tedeschiamelia@gmail.com>
2022-06-22 16:21:34 +0200

    Command-line art

--- ECMAScript/sum.mjs
+++ ECMAScript/sum.mjs
@@ -1,5 +1,6 @@
+import clc from "cli-color"
 let sum = (a, b) => a + b
 
-console.log ("The sum is: " + sum(5, 6))
+console.log (clc.yellow("The sum is: " + sum(5, 6)))
 
 export default sum
--- commonJs/sum.js
+++ commonJs/sum.js
@@ -1,5 +1,7 @@
+const clc = require("cli-color")
+
 let sum = (a, b) => a + b
 
-console.log ("The sum is: " + sum(5, 6))
+console.log (cli.yellow("The sum is: " + sum(5, 6)))
 
 module.exports = sum
